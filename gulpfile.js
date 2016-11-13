b10a197517097e3b38c9b0006d1ee2d28f40385f
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-14 00:27:26 +0800

    格式化代码

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 // Node modules
-var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = require('chalk'), es = require('event-stream');
+var fs = require('fs'),
+    vm = require('vm'),
+    merge = require('deeply'),
+    chalk = require('chalk'),
+    es = require('event-stream');
 
 // Gulp and plugins
-var gulp = require('gulp'), rjs = require('gulp-requirejs-bundler'), concat = require('gulp-concat'), clean = require('gulp-clean'),
-    replace = require('gulp-replace'), uglify = require('gulp-uglify'), htmlreplace = require('gulp-html-replace'), typescript = require('gulp-tsc'), webServer = require('gulp-webserver'), gutil = require('gulp-util');
-
-gulp.task('test', ['ts'], function () {
-    new KarmaServer({
-        configFile: __dirname + '/karma.conf.js',
-        action: 'run'
-    }).start();
-});
+var gulp = require('gulp'),
+    rjs = require('gulp-requirejs-bundler'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean'),
+    replace = require('gulp-replace'),
+    uglify = require('gulp-uglify'),
+    htmlreplace = require('gulp-html-replace'),
+    typescript = require('gulp-tsc'),
+    webServer = require('gulp-webserver'),
+    gutil = require('gulp-util');
 
 // WebServer
 gulp.task('web-server', ['auto-ts'], function() {
@@ -27,7 +32,14 @@ gulp.task('web-server', ['auto-ts'], function() {
         }));
 });
 
-gulp.task('auto-ts', ['ts'], function(){
+gulp.task('test', ['ts'], function() {
+    new KarmaServer({
+        configFile: __dirname + '/karma.conf.js',
+        action: 'run'
+    }).start();
+});
+
+gulp.task('auto-ts', ['ts'], function() {
     gulp.watch('**/*.ts', function() {
         gulp.src(['**/*.ts'])
             .pipe(typescript({
@@ -56,16 +68,9 @@ requireJsOptimizerConfig = merge(requireJsRuntimeConfig, {
         'text!components/about-page/about.html'
     ],
     insertRequire: ['app/startup'],
-    bundles: {
-        // If you want parts of the site to load on demand, remove them from the 'include' list
-        // above, and group them into bundles here.
-        // 'bundle-name': [ 'some/module', 'another/module' ],
-        // 'another-bundle-name': [ 'yet-another-module' ]
-    }
+    bundles: {}
 });
 
-
-// Compile all .ts files, producing .js and source map files alongside them
 gulp.task('ts', function() {
     return gulp.src(['**/*.ts'])
         .pipe(typescript({
@@ -77,25 +82,26 @@ gulp.task('ts', function() {
         .pipe(gulp.dest('./'));
 });
 
-// Discovers all AMD dependencies, concatenates together all required .js files, minifies them
-gulp.task('js', ['ts'], function () {
+gulp.task('js', ['ts'], function() {
     return rjs(requireJsOptimizerConfig)
-        .pipe(uglify({ preserveComments: 'some' }))
+        .pipe(uglify({
+            preserveComments: 'some'
+        }))
         .pipe(gulp.dest('./dist/'));
 });
 
-// Concatenates CSS files, rewrites relative paths to Bootstrap fonts, copies Bootstrap fonts
-gulp.task('css', function () {
+gulp.task('css', function() {
     var bowerCss = gulp.src('./src/front/bower_modules/components-bootstrap/css/bootstrap.min.css')
         .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/')),
         appCss = gulp.src('./src/front/css/*.css'),
         combinedCss = es.concat(bowerCss, appCss).pipe(concat('css.css')),
-        fontFiles = gulp.src('./src/front/bower_modules/components-bootstrap/fonts/!*', { base: './src/front/bower_modules/components-bootstrap/' });
+        fontFiles = gulp.src('./src/front/bower_modules/components-bootstrap/fonts/!*', {
+            base: './src/front/bower_modules/components-bootstrap/'
+        });
     return es.concat(combinedCss, fontFiles)
         .pipe(gulp.dest('./dist/'));
 });
 
-// Copies index.html, replacing <script> and <link> tags to reference production URLs
 gulp.task('html', function() {
     return gulp.src('./src/front/index.html')
         .pipe(htmlreplace({
@@ -105,14 +111,16 @@ gulp.task('html', function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-// Removes all files from ./dist/, and the .js/.js.map files compiled from .ts
 gulp.task('clean', function() {
-    var distContents = gulp.src('./dist/**/*', { read: false }),
-        generatedJs = gulp.src(['./src/front/**/*.js', './src/front/**/*.js.map', 'test/**/*.js', 'test/**/*.js.map'], { read: false })
-            .pipe(es.mapSync(function(data) {
-                // Include only the .js/.js.map files that correspond to a .ts file
-                return fs.existsSync(data.path.replace(/\.js(\.map)?$/, '.ts')) ? data : undefined;
-            }));
+    var distContents = gulp.src('./dist/**/*', {
+            read: false
+        }),
+        generatedJs = gulp.src(['./src/front/**/*.js', './src/front/**/*.js.map', 'test/**/*.js', 'test/**/*.js.map'], {
+            read: false
+        })
+        .pipe(es.mapSync(function(data) {
+            return fs.existsSync(data.path.replace(/\.js(\.map)?$/, '.ts')) ? data : undefined;
+        }));
     return es.merge(distContents, generatedJs).pipe(clean());
 });
 
