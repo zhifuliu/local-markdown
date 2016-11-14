c9268ff7bf3d5dcb1898517faad606adbda0fcf1
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-14 19:05:04 +0800

    登录接口完成

--- src/backend/public/api/login.js
+++ src/backend/public/api/login.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
+var userBase = require('../components/userbase');
 
 router.post('/', function(req, res) {
     if (!_.isUndefined(req.body.user) && !_.isUndefined(req.body.pass)) {
-        console.log(req.body);
+        res.send(userBase.login(req.body.user, req.body.pass));
     } else {
         res.send({
-            errCode: 1,
+            errCode: -1,
             errMsg: 'params err'
         });
     }
