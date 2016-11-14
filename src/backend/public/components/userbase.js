64adabeba439034fede6e9a9e6ccb073ccb270c9
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-14 19:04:35 +0800

    用户信息操作：用户是否存在；登录函数

---
+++ src/backend/public/components/userbase.js
@@ -0,0 +1,45 @@
+var _ = require('underscore');
+var users = require('../../user.json').users;
+
+function haveUser(userId) {
+    return _.find(users, function(user) {
+        return user.id == userId;
+    }) != undefined;
+}
+
+function login (userId, pass) {
+    var result = {};
+    if (haveUser(userId)) {
+        var temp = _.find(users, function(user) {
+           return user.id == userId && user.pass == pass;
+       });
+       if (temp != undefined) {
+           result = {
+               errCode: 1,
+               errMsg: '登录成功',
+               data: {
+                  id: temp.id,
+                  nickname: temp.nickname
+               }
+           }
+       } else {
+           result = {
+               errCode: -1,
+               errMsg: '密码错误'
+           }
+       }
+    } else {
+        result = {
+            errCode: -1,
+            errMsg: '用户不存在'
+        }
+    }
+    return result;
+}
+
+function getUserMsg(userId) {
+
+}
+
+exports.getUserMsg = getUserMsg;
+exports.login = login;
