a3c0cf8e2065dd92de75757b64866c89370c9382
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-14 18:29:55 +0800

    允许跨域访问，swagger ui 访问需要跨域，并没有用代理

--- src/backend/app.js
+++ src/backend/app.js
@@ -30,7 +30,17 @@ app.use(
     })
 );
 // app.use(express.static(path.join(__dirname, 'test')));
-
+// express.js 允许跨域
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (req.method == 'OPTIONS') {
+        res.send(200); // 让options请求快速返回
+    } else {
+        next();
+    }
+});
 //api
 app.use('/api/login', require('./public/api/login'));
 // app.use('/api/login', loginFilter, require('./public/api/login'));
