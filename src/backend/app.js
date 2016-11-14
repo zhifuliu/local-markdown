266d58d0f5a9610f815e5bf7d2db02086c5b06ac
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-14 15:27:30 +0800

    工程运行调试

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,10 +9,6 @@ var loginFilter = require('./public/filter/loginFilter');
 
 var app = express();
 
-// view engine setup,use jade engine
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.json()); // 为了能获取到参数
 app.use(bodyParser.urlencoded({
@@ -25,7 +21,7 @@ app.use(cookieParser('', {
 // 使用  session 中间件
 app.use(
     session({
-        secret: 'collect', // 建议使用 128 个字符的随机字符串。用于计算 hash 值并存放在 cookies 中
+        secret: 'locale_markdown_write', // 建议使用 128 个字符的随机字符串。用于计算 hash 值并存放在 cookies 中
         cookie: {
             maxAge: 2 * 60 * 1000
         },
@@ -35,19 +31,9 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, 'public')));
 
-// express.js 允许跨域
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    if (req.method == 'OPTIONS') {
-        res.send(200); // 让options请求快速返回
-    } else {
-        next();
-    }
-});
 //api
-app.use('/api/login', loginFilter, require('./public/api/login'));
+app.use('/api/login', require('./public/api/login'));
+// app.use('/api/login', loginFilter, require('./public/api/login'));
 
 
 /// catch 404 and forward to error handler
@@ -59,10 +45,5 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.send(err.message);
 });
-
-// 如果添加这一行,那么可以直接通过 node app.js 来运行应用,但是通过 gulp 那么就会报错,估计是端口冲突
-// app.listen(3000, function() {
-//   console.log('collect-node listening on port 3000!');
-// });
 
 module.exports = app;
