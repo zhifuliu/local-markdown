47ce41392f0aa5e2de1a500304b3280733c1253e
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-15 10:40:44 +0800

    允许跨域的请求头，添加 app_key 参数

--- src/backend/app.js
+++ src/backend/app.js
@@ -34,7 +34,7 @@ app.use(
 // express.js 允许跨域
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild, api_key');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     if (req.method == 'OPTIONS') {
         res.send(200); // 让options请求快速返回
