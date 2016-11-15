3e8093ed6edbd7f292d5e190c6b0ae56b6a083fb
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-15 18:45:30 +0800

    退出登录接口，只操作 session 信息

--- src/backend/public/api/logout.js
+++ src/backend/public/api/logout.js
@@ -3,26 +3,11 @@ var router = express.Router();
 var _ = require('underscore');
 
 router.post('/', function(req, res) {
-    if (_.isUndefined(req.session.userMsg)) {
-        if (_.isUndefined(req.cookies.userMsg) || req.cookies.userMsg == 'undefined') {
-            res.send({
-                errCode: 0,
-                eddMsg: 'no login'
-            });
-        } else {
-            req.session.userMsg = undefined;
-            res.cookie('userMsg', undefined);
-            res.send({
-                errCode: 1,
-                errMsg: 'logout success'
-            });
-        }
-    } else {
-        res.send({
-            errCode: 0,
-            eddMsg: 'no login'
-        });
-    }
+    req.session.userMsg = undefined;
+    res.send({
+        errCode: 1,
+        errMsg: 'logout success'
+    });
 });
 
 module.exports = router;
