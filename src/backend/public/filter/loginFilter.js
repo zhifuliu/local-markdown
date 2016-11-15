2a6357bfc5f7277f9533e18f6c360ef2d5894c2d
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-15 18:43:13 +0800

    登录过滤器只判断 session, 不判断 cookie

--- src/backend/public/filter/loginFilter.js
+++ src/backend/public/filter/loginFilter.js
@@ -1,22 +1,11 @@
 var _ = require('underscore');
 
 module.exports = function(req, res, next) {
-    console.log('request session:');
-    console.log(req.session);
-    console.log('request cookies:');
-    console.log(req.cookies);
     if (_.isUndefined(req.session.userMsg)) {
-        if (_.isUndefined(req.cookies.userMsg) || req.cookies.userMsg == 'undefined') {
-            res.send({
-                errCode: -1,
-                eddMsg: 'no login'
-            });
-        } else {
-            res.send({
-                errCode: 1,
-                eddMsg: 'login'
-            });
-        }
+        res.send({
+            errCode: -1,
+            eddMsg: 'no login'
+        });
     } else {
         next();
     }
