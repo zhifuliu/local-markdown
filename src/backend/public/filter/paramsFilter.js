cb4d53819352c6c614c3da67de8ac0c4258519c1
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-15 10:42:21 +0800

    参数转换过滤器。打印 params 和 query 中的参数

--- src/backend/public/filter/paramsFilter.js
+++ src/backend/public/filter/paramsFilter.js
@@ -2,12 +2,16 @@ var _ = require('underscore');
 
 module.exports = function(req, res, next) {
     if (!_.isEmpty(req.params)) {
+        console.log('req.params:');
         _.each(_.keys(req.params), function(key) {
+            console.log(key + ' : ' + req.params[key]);
             req.body[key] = req.params[key];
         });
     }
     if (!_.isEmpty(req.query)) {
+        console.log('req.query:');
         _.each(_.keys(req.query), function(key) {
+            console.log(key + ' : ' + req.query[key]);
             req.body[key] = req.query[key];
         });
     }
