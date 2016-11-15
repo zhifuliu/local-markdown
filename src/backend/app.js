5da5a3a7e57aeb21a7e087b69805d9e2c5307ad3
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-15 18:01:56 +0800

    swagger ui 可以允许带 cookie,方法：enableCookies: true, 但是在登录的时候报跨域问题，需要添加 Access-Control-Allow-Credentials 为 true

--- src/backend/app.js
+++ src/backend/app.js
@@ -36,6 +36,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:9001');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild, api_key');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
     if (req.method == 'OPTIONS') {
         res.send(200); // 让options请求快速返回
     } else {
