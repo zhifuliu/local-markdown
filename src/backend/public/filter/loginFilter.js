0dc621d2b6b535614c466b278aafc4c5bf3b9d61
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-16 08:34:03 +0800

    登录过滤器，未登录状态，响应码设置为 401

--- src/backend/public/filter/loginFilter.js
+++ src/backend/public/filter/loginFilter.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 
 module.exports = function(req, res, next) {
     if (_.isUndefined(req.session.userMsg)) {
-        res.send({
+        res.status(401).send({
             errCode: -1,
             eddMsg: 'no login'
         });
