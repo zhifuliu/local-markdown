2dbbd791eb1a93a87c24d696d954ee47fc4d2689
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-16 10:09:19 +0800

    添加将数据存入 json 文件的接口

---
+++ src/backend/public/components/fileOperation.js
@@ -0,0 +1,39 @@
+var fs = require('fs'),
+    path = require('path');
+
+function writeJsonFile(filepath, data) {
+    var ws = fs.createWriteStream(filepath);
+    ws
+        .on('error', function(err) {
+            console.log(err);
+        })
+        .on('data', function(chunk) {
+            console.log('writeStream data event');
+        })
+        .on('end', function() {
+            console.log('writeStream end');
+        })
+        .on('event', function(msg) {
+            console.log(msg);
+        });
+        ws.once('open', function(feedback) {
+            try {
+                ws.write(new Buffer(data, 'utf-8'));
+                ws.end();
+            } catch(error) {
+                console.log(error);
+            }
+        });
+}
+function writeJsonFileBackup(filepath, data) {
+    fs.writeFile(filepath, data, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('save success');
+        }
+    });
+}
+
+exports.writeJsonFile = writeJsonFile;
