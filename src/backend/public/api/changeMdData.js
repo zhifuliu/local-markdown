1b4278a39b2fb0d84d25b864632f4106b202d00e
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-17 11:42:18 +0800

    nodegit 尝试

--- src/backend/public/api/changeMdData.js
+++ src/backend/public/api/changeMdData.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var fs = require('fs');
+var Git = require('nodegit');
 var router = express.Router();
 var _ = require('underscore');
+
 var userBase = require('../components/userBase');
 var productBase = require('../components/productBase');
 var fileOperation = require('../components/fileOperation');
@@ -12,12 +14,12 @@ router.post('/', function(req, res) {
     if (!_.isUndefined(req.body.projectName) && !_.isUndefined(req.body.projectUrl) && !_.isUndefined(req.body.mdFileName) && !_.isUndefined(req.body.newData)) {
         try {
             // fileOperation.writeFile(req.body.projectUrl + '/' + (req.body.mdPath.length == 0 ? '' : (req.body.mdPath + '/')) + req.body.mdFileName, req.body.newData);
-            fs.writeFileSync(req.body.projectUrl + '/' + (req.body.mdPath.length == 0 ? '' : (req.body.mdPath + '/')) + req.body.mdFileName, req.body.newData, 'utf-8', function (err, data) {});
+            fs.writeFileSync(req.body.projectUrl + '/' + (req.body.mdPath.length == 0 ? '' : (req.body.mdPath + '/')) + req.body.mdFileName, req.body.newData, 'utf-8', function(err, data) {});
             res.send({
                 errCode: 1,
                 errMsg: 'success'
             });
-        } catch(err) {
+        } catch (err) {
             res.send({
                 errCode: -1,
                 errMsg: err
