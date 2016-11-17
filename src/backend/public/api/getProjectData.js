a31bc957493f480149b108fc1d5717697e5a3339
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-17 09:23:15 +0800

    修改获取扫面数据接口，不适用 require,fs.readFileSync 来读取文件数据

--- src/backend/public/api/getProjectData.js
+++ src/backend/public/api/getProjectData.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var _ = require('underscore');
 var userBase = require('../components/userBase');
@@ -7,13 +8,15 @@ var fileOperation = require('../components/fileOperation');
 
 router.post('/', function(req, res) {
     if (!_.isUndefined(req.body.name) && !_.isUndefined(req.body.url)) {
+        // JSON.parse(fs.readFileSync('./src/backend/public/data/' + req.body.name + '.json', 'utf-8'))
+        // require('../data/' + req.body.name + '.json')
         try {
             res.send({
                 errCode: 1,
                 errMsg: 'success',
-                data: require('../data/' + req.body.name + '.json')
+                data: JSON.parse(fs.readFileSync('./src/backend/public/data/' + req.body.name + '.json', 'utf-8'))
             });
-        } catch (err) {
+        } catch(err) {
             res.send({
                 errCode: -1,
                 errMsg: err
